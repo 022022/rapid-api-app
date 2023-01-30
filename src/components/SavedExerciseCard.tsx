be4f33d13685b0603c8e9bf206a7e0a6a7685760
Zhenya <evgeegai@gmail.com>
2023-01-30 10:51:37 +0300

    style Saved Exercises page

--- src/components/SavedExerciseCard.tsx
+++ src/components/SavedExerciseCard.tsx
@@ -2,30 +2,47 @@ import { nanoid } from 'nanoid';
 import { SavedExercise } from '../types/types';
 
 function SavedExerciseCard( {data}: {data: SavedExercise} ){
-  return <div className="card bg-base-100 shadow-xl">
-          <h2 className="card-title">{data.name}  </h2>
-          <figure>
-            <div className='flex xs:gap-0 sm:gap-4 flex-wrap '>
-              <img className="w-60" alt={data.name} src={
-                data.images[0]
-              }/>
-              <img className="w-60" alt={data.name} src={
-                data.images[1]
-              }/>
-            </div>
-          </figure>
-          <div className="card-body">
-            <div className="card-actions justify-start h-full content-end">
-              <div className="badge badge-outline">{data.difficulty}</div>
-              <div className="badge badge-outline">{data.type}</div>
-              <div className="badge badge-outline">{data.muscle}</div>
+  return <>
+
+  <div className='collapse collapse-arrow border-b'>
+				<input type='checkbox' />
+				<div className='collapse-title text-xl font-medium'>
+          <h2 className='card-title'>{data.name} </h2>
+				</div>
+				<div className='collapse-content'>
+
+            <div className='flex xs:gap-0 sm:gap-4 flex-wrap px-5 sm:px-0'>
+              <img
+                className='w-60'
+                alt={data.name}
+                src={data.images[0]}
+              />
+              <img
+                className='w-60'
+                alt={data.name}
+                src={data.images[1]}
+              />
             </div>
 
-            <ul className="text-left">
-              { data.instructions && data.instructions?.split('. ').map((item) => <li key={nanoid()}>{item}</li>) }
+          <div className='card-body'>
+            <div className='card-actions justify-start h-full content-end'>
+              <div className='badge badge-outline'>
+                {data.difficulty}
+              </div>
+              <div className='badge badge-outline'>{data.type}</div>
+              <div className='badge badge-outline'>{data.muscle}</div>
+            </div>
+
+            <ul className='text-left'>
+              {data.instructions &&
+                data.instructions
+                  ?.split('. ')
+                  .map((item) => <li key={nanoid()}>{item}</li>)}
             </ul>
           </div>
-        </div>
+				</div>
+			</div>
+  </>
 }
 
 export default SavedExerciseCard;
